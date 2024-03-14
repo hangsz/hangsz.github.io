19f5d98e75fa4a31c7cc8530989fc0139da4d647
hang719 <zhenhang.sun@gmail.com>
2024-03-14 21:03:41 +0800

    add

---
+++ main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+'''
+@File    :   main.py
+@Time    :   2022/05/27 22:23:38
+@Author  :   https://github.com/hangsz
+@Version :   0.1.0
+@Contact :   zhenhang.sun@gmail.com
+'''
+
+
+import os 
+import sys
+
+def main()-> int:
+    os.system('hugo')
+
+    os.system('git add .')
+    os.system('git commit -m "add"')
+    os.system('git push origin main')
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
